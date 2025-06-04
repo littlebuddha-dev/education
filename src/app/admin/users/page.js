27dd6318dc38a08afeaae91435eb195b1dbef9dd
littlebuddha <58803643+littlebuddha-dev@users.noreply.github.com>
2025-06-04 12:59:10 +0900

    Update page.js
    bug fix

--- src/app/admin/users/page.js
+++ src/app/admin/users/page.js
@@ -4,6 +4,7 @@
 import { useEffect, useState } from 'react';
 import { jwtDecode } from 'jwt-decode';
 import { useRouter } from 'next/navigation';
+import { getCookie } from '@/utils/authUtils';
 
 export default function AdminUsersPage() {
   const [users, setUsers] = useState([]);
